3a9e986ac875f00e027538020c9a0a45b155c3a2
liuxiansheng2016 <2542004302@qq.com>
2025-01-09 12:02:39 +0800

    deploy to git

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export', // 启用静态导出
+    images: {
+        unoptimized: true, // GitHub Pages 不支持图片优化
+    },
+    basePath: process.env.NODE_ENV === 'production' ? '/blog' : '', // 替换 'blog' 为你的仓库名
+}
+
+module.exports = nextConfig
