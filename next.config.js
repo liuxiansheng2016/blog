bbdfefa3d2b3af495c6a08fd151a8afe1afb0cb0
liuxiansheng2016 <2542004302@qq.com>
2025-01-10 09:34:37 +0800

    fix build issue

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
-    basePath: process.env.NODE_ENV === 'production' ? '/blog' : '', // 只需要仓库名
+    basePath: process.env.NODE_ENV === 'production' ? '/blog' : '',
 }
 
 module.exports = nextConfig
