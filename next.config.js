e953b019ccdb1333f372056a4e1bc021b1241878
liuxiansheng2016 <2542004302@qq.com>
2025-01-10 09:24:51 +0800

    fix basePath issue

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
         unoptimized: true, // GitHub Pages 不支持图片优化
     },
     basePath: process.env.NODE_ENV === 'production' ?
-        'https://github.com/liuxiansheng2016/blog' :
+        '/https://github.com/liuxiansheng2016/blog' :
         '', // 替换 'blog' 为你的仓库名
 }
 
