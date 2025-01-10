90c6b4407289e2ca42a8142834a7e0e25dae3c8d
liuxiansheng2016 <2542004302@qq.com>
2025-01-10 09:22:34 +0800

    update basePath

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ const nextConfig = {
     images: {
         unoptimized: true, // GitHub Pages 不支持图片优化
     },
-    basePath: process.env.NODE_ENV === 'production' ? '/blog' : '', // 替换 'blog' 为你的仓库名
+    basePath: process.env.NODE_ENV === 'production' ?
+        'https://github.com/liuxiansheng2016/blog' :
+        '', // 替换 'blog' 为你的仓库名
 }
 
 module.exports = nextConfig
